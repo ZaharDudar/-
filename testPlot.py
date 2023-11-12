31647b43e03b466d78a501673f53294f9920bb9d
ZaharDudar <zahar5070@gmail.com>
2023-11-12 20:10:14 +0300

    димка, всё для тебя

--- testPlot.py
+++ testPlot.py
@@ -8,8 +8,30 @@ for name in [f"./{i}mm.csv" for i in range(0,80,10)]:
     with open(name, 'r') as f:
         lines = [j.strip().split(',') for j in f.readlines()]
         data = np.array([[float(line[0]),900-float(line[1])] for line in lines])
-    print(lines)
-    plt.plot(data[:,0], data[:,1],label=name)
+
+    # print(data)
+    max_x, min_x=0,max(data[:,0])
+    for ind in range(len(data[:,0])):
+        if data[ind,1] > (max(data[:,1])-10):
+            # print(data[ind])
+            max_x =max(data[ind,0],max_x)
+            min_x =min(data[ind,0],min_x)
+
+    data[:,0]=data[:,0] - ((max_x+min_x)/2)
+
+    new_data=[]
+    for i in range(len(data)-1,0,-1):
+        if 262 > data[i,0] >= 0:
+            new_data.append([-data[i,0],data[i,1]])
+        if data[i,1] < 0:
+            break
+    for i in range(len(new_data)-1,0,-1):
+        new_data.append([-new_data[i][0],new_data[i][1]])
+    
+    new_data=np.array(new_data)
+    # print(new_data)
+    plt.plot(new_data[:,0], new_data[:,1],label=name[2:-4])
+
 plt.grid()
 plt.legend()
 plt.show()
